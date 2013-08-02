f2abe5a4523625d3e341acc316fff554689269b1
est77 <ramenhdr@gmail.com>
2013-08-03 01:22:26 +0200

    collected info for windows sys info

--- ramen/os/ramen/os/implementation/system_info_win.hpp
+++ ramen/os/ramen/os/implementation/system_info_win.hpp
@@ -34,6 +34,87 @@ struct system_info_t::impl
 {
     explicit impl( system_info_t& self)
     {
+        /*
+            BOOL WINAPI GetUserName( _Out_    LPTSTR lpBuffer,
+                                     _Inout_  LPDWORD lpnSize);
+        */
+        /*
+            // executable path
+            DWORD WINAPI GetModuleFileName( _In_opt_  HMODULE hModule,
+                                            _Out_     LPTSTR lpFilename,
+                                            _In_      DWORD nSize);
+        */
+        /*
+            #include "Userenv.h"
+            #pragma comment(lib, "userenv.lib")
+            CString GetUserHomeDir()
+            {
+               TCHAR szHomeDirBuf[MAX_PATH] = { 0 };
+
+               HANDLE hToken = 0;
+               VERIFY( OpenProcessToken( GetCurrentProcess(), TOKEN_QUERY, &hToken ));
+
+               DWORD BufSize = MAX_PATH;
+               VERIFY( GetUserProfileDirectory( hToken, szHomeDirBuf, &BufSize ));
+
+               CloseHandle( hToken );
+               return CString( szHomeDirBuf );
+            }
+        */
+        /*
+            // RAM Size
+
+            //  Sample output:
+            //  There is       51 percent of memory in use.
+            //  There are 2029968 total KB of physical memory.
+            //  There are  987388 free  KB of physical memory.
+            //  There are 3884620 total KB of paging file.
+            //  There are 2799776 free  KB of paging file.
+            //  There are 2097024 total KB of virtual memory.
+            //  There are 2084876 free  KB of virtual memory.
+            //  There are       0 free  KB of extended memory.
+
+            #include <windows.h>
+            #include <stdio.h>
+            #include <tchar.h>
+
+            // Use to convert bytes to KB
+            #define DIV 1024
+
+            // Specify the width of the field in which to print the numbers.
+            // The asterisk in the format specifier "%*I64d" takes an integer
+            // argument and uses it to pad and right justify the number.
+            #define WIDTH 7
+
+            void _tmain()
+            {
+              MEMORYSTATUSEX statex;
+
+              statex.dwLength = sizeof (statex);
+
+              GlobalMemoryStatusEx (&statex);
+
+              _tprintf (TEXT("There is  %*ld percent of memory in use.\n"),
+                        WIDTH, statex.dwMemoryLoad);
+              _tprintf (TEXT("There are %*I64d total KB of physical memory.\n"),
+                        WIDTH, statex.ullTotalPhys/DIV);
+              _tprintf (TEXT("There are %*I64d free  KB of physical memory.\n"),
+                        WIDTH, statex.ullAvailPhys/DIV);
+              _tprintf (TEXT("There are %*I64d total KB of paging file.\n"),
+                        WIDTH, statex.ullTotalPageFile/DIV);
+              _tprintf (TEXT("There are %*I64d free  KB of paging file.\n"),
+                        WIDTH, statex.ullAvailPageFile/DIV);
+              _tprintf (TEXT("There are %*I64d total KB of virtual memory.\n"),
+                        WIDTH, statex.ullTotalVirtual/DIV);
+              _tprintf (TEXT("There are %*I64d free  KB of virtual memory.\n"),
+                        WIDTH, statex.ullAvailVirtual/DIV);
+
+              // Show the amount of extended memory available.
+
+              _tprintf (TEXT("There are %*I64d free  KB of extended memory.\n"),
+                        WIDTH, statex.ullAvailExtendedVirtual/DIV);
+            }
+        */
     }
 };
 
