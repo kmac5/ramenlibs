8fcb2cf0c16eef9bad221f5a2a918d43f0765d5b
est77 <ramenhdr@gmail.com>
2013-10-19 06:39:17 +0200

    Added FindArnold.cmake module

---
+++ cmake/Modules/FindArnold.cmake
@@ -0,0 +1,42 @@
+# Copyright (c) 2013 Esteban Tovagliari
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
+# Find Arnold headers and libraries.
+#
+#  ARNOLD_INCLUDE_DIRS - where to find Arnold includes.
+#  ARNOLD_LIBRARIES    - List of libraries when using Arnold.
+#  ARNOLD_FOUND        - True if Arnold found.
+
+FIND_PATH( ARNOLD_INCLUDE_DIR NAMES ai_api.h)
+FIND_LIBRARY( ARNOLD_LIBRARY NAMES kick)
+
+INCLUDE( FindPackageHandleStandardArgs)
+FIND_PACKAGE_HANDLE_STANDARD_ARGS( ARNOLD DEFAULT_MSG ARNOLD_LIBRARY ARNOLD_INCLUDE_DIR)
+
+# Copy the results to the output variables.
+IF( ARNOLD_FOUND)
+    SET( ARNOLD_LIBRARIES ${ARNOLD_LIBRARY})
+    SET( ARNOLD_INCLUDE_DIRS ${ARNOLD_INCLUDE_DIR})
+ELSE()
+    SET( ARNOLD_LIBRARIES)
+    SET( ARNOLD_INCLUDE_DIRS)
+ENDIF()
+
+MARK_AS_ADVANCED( ARNOLD_INCLUDE_DIR ARNOLD_LIBRARY)
